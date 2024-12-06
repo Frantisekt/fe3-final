593505d75376fa19fd0943040b7db46ae99e27cd
Frantisekt <frankoba577@gmail.com>
2024-12-05 20:30:41 -0500

    fix images routes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -28,9 +28,9 @@ const Card = ({ name, username, id, image, onRemoveFav }) => {
         <p>{username}</p>
         
       </Link>
-      <button onClick={addFav} className="favButton"><img src="public\favicon.ico" style={{ width: '20px', height: '20px' }} /></button>
+      <button onClick={addFav} className="favButton"><img src="/favicon.ico" style={{ width: '20px', height: '20px' }} /></button>
       {location.pathname === '/favs' && (
-     <button onClick={removeFav} className="removeButton"><img src="public\images\trash.png" style={{ width: '20px', height: '20px' }} /> </button>
+     <button onClick={removeFav} className="removeButton"><img src="/images/trash.png" style={{ width: '20px', height: '20px' }} /> </button>
       )}
     </div>
   );
