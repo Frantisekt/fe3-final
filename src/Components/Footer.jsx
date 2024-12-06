b166a536cc2f10a264c0fa8f1e114f925062ec8f
Frantisekt <frankoba577@gmail.com>
2024-12-05 21:24:43 -0500

    add sm icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
+
+
 const Footer = () => {
   return (
     <footer>
+      <div>
         <p>Powered by</p>
-        <img src="./images/DH.png" alt='DH-logo' />
+        <a href="https://www.digitalhouse.com/" target="_blank" >
+          <img src="./images/DH.png" alt='DH-logo' />
+        </a>
+      </div>
+      
+      <div className="icon-container">
+      <a href="https://www.instagram.com/_digitalhouse/" target="_blank" >
+          <img className="icon" src="./images/ico-instagram2.png" alt="Instagram" />
+        </a>
+        <a href="https://www.facebook.com/DigitalHousePeru/" target="_blank" >
+          <img className="icon" src="./images/ico-facebook2.png" alt="Facebook" />
+        </a>
+        <a href="https://www.tiktok.com" target="_blank" >
+          <img className="icon" src="./images/ico-tiktok2.png" alt="TikTok" />
+        </a>
+        <a href="https://www.whatsapp.com" target="_blank">
+          <img className="icon" src="./images/ico-whatsapp2.png" alt="WhatsApp" />
+        </a>
+      </div>
     </footer>
   )
 }
